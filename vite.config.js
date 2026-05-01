e57b9b5f491d6787dcfb39290879caf831e77e9f
Jammie <jm21459n@pace.edu>
2026-05-01 13:39:01 -0400

    update config files

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "Jamm1e/project3-Jamm1e", 
+  base: "/project3-Jamm1e/", 
 })
